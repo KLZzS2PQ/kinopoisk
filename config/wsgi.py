c0e9a336aa6c4713428cd55801eb9cc3b1748f12
KLZzS2PQ <kicaevartem5@gmail.com>
2024-04-02 18:46:16 +0300

    process

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
+# -*- coding: utf-8 -*-
 
 import os
+import sys
+import platform
+
+# путь к проекту
+sys.path.insert(0, '/home/c/ch45181/django_to35t/public_html')
+# путь к фреймворку
+sys.path.insert(0, '/home/c/ch45181/django_to35t/public_html/config')
+# путь к виртуальному окружению
+python_version = ".".join(platform.python_version_tuple()[:2])
+sys.path.insert(0, '/home/c/ch45181/django_to35t//django/lib/python{0}/site-packages'.format(python_version))
+os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings"
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = get_wsgi_application()
